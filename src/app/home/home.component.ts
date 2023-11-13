8fd9ec1ce546313268669f51308afd99237cb045
UmitAkbas <umit.akbas@hotmail.com.tr>
2023-11-13 06:49:16 +0300

    validators added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import { FormGroup,FormBuilder } from '@angular/forms';
+import { FormGroup,FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +13,19 @@ submitted= false;
 constructor(private formBuilder: FormBuilder) {}
 
 ngOnInit():void{
-  
+  this.intializeForm();
 }
 
 intializeForm(){
   this.userForm=this.formBuilder.group({
-
+    name:["",[Validators.required,Validators.minLength(3),Validators.maxLength(15)]]
   })
+}
+
+submitForm(){
+  this.submitted=true;
+  if(this.userForm.valid){
+    console.log(this.userForm.value)
+  }
 }
 }
